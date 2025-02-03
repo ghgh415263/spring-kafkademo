fc437505c34d0957eb1f4bfd8e1f0c821c7065ba
kjy1994 <ghgh415263@naver.com>
2025-02-03 22:34:36 +0900

    EmbeddedKafkaHolder를 테스트 코드에서 사용하도록 변경

--- src/test/java/com/example/springkafkademo/kafka/KafkaTest.java
+++ src/test/java/com/example/springkafkademo/kafka/KafkaTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 
 import java.util.Map;
@@ -17,27 +16,27 @@ import java.util.Properties;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@EmbeddedKafka
 public class KafkaTest {
+
+    private static final EmbeddedKafkaBroker kafkaBroker = EmbeddedKafkaHolder.getEmbeddedKafka();
 
     private final static String TEST_TOPIC = "test-topic";
 
     @Test
-    public void test(EmbeddedKafkaBroker broker) {
+    public void test() {
         // 토픽 추가
-        broker.addTopics(TEST_TOPIC);
-        sendTestMessages(broker);
+        kafkaBroker.addTopics(TEST_TOPIC);
+        sendTestMessages(kafkaBroker);
 
         // consumer 생성, 100개씩 가져오게
-        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", broker);
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", kafkaBroker);
         consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         consumerProps.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
         ConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
         Consumer<String, String> consumer = cf.createConsumer();
-        broker.consumeFromAnEmbeddedTopic(consumer, TEST_TOPIC);
+        kafkaBroker.consumeFromAnEmbeddedTopic(consumer, TEST_TOPIC);
         ConsumerRecords<String, String> replies = KafkaTestUtils.getRecords(consumer);
         consumer.close();
-        broker.destroy();
 
         assertThat(replies.count()).isEqualTo(100);
     }
