aff0d1f8ce19bec8a03780f9a3f15ce32309b99c
kjy1994 <ghgh415263@naver.com>
2025-02-03 22:34:23 +0900

    EmbeddedKafkaHolder를 테스트 코드에서 사용하도록 변경

---
+++ src/test/java/com/example/springkafkademo/kafka/EmbeddedKafkaHolder.java
@@ -0,0 +1,29 @@
+package com.example.springkafkademo.kafka;
+
+import org.springframework.kafka.KafkaException;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+
+public final class EmbeddedKafkaHolder {
+
+    private static EmbeddedKafkaBroker embeddedKafka = new EmbeddedKafkaBroker(1);
+
+    private static boolean started;
+
+    public static EmbeddedKafkaBroker getEmbeddedKafka() {
+        if (!started) {
+            try {
+                embeddedKafka.afterPropertiesSet();
+            }
+            catch (Exception e) {
+                throw new KafkaException("Embedded broker failed to start", e);
+            }
+            started = true;
+        }
+        return embeddedKafka;
+    }
+
+    private EmbeddedKafkaHolder() {
+        super();
+    }
+
+}
